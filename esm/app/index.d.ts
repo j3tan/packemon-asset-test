92981bdaca02dd6608d4f9a28a1dc4bc130a33c3
Jeff Tan <jeff.tan@coinbase.com>
2023-03-06 09:53:45 -0800

    gitignore

--- esm/app/index.d.ts
+++
@@ -1,3 +0,0 @@
-import './app.css';
-export declare function app(): void;
-//# sourceMappingURL=index.d.ts.map
--- esm/index.js
+++
@@ -1,9 +0,0 @@
-// Bundled with Packemon: https://packemon.dev
-// Platform: browser, Support: stable, Format: esm
-
-import 'app.css';
-function app() {
-  console.log('app started');
-}
-export { app };
-//# sourceMappingURL=index.js.map
